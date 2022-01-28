3151af6e162a69fe5d1fac30117ad1c30622efc5
ElenaLuv341 <olena.peschanskaya341@gmail.com>
2022-01-28 18:45:22 +0200

    commit

--- cypress/integration/web-driveruni/contact_us.js
+++ cypress/integration/web-driveruni/contact_us.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 describe('Test Contact Us form via WebdriverUni', () => {
-    it('Should be able to submit a successful submission via contact us form', () => {
+    it.only('Should be able to submit a successful submission via contact us form', () => {
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type('Joe');
