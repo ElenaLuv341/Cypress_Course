1c4f4268798a221a87fbeff2a4c8d720d8fa7767
ElenaLuv341 <olena.peschanskaya341@gmail.com>
2022-02-01 18:57:14 +0200

    c

---
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('Inspect Automation test store items using chain commands', () => {
+    it('Click on the first item using item header', () => {
+        cy.visit('https://www.automationteststore.com/')
+        cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
+    })
+    it('Click on the first item using item text', () => {
+        cy.visit('https://www.automationteststore.com/')
+        cy.get('.prdocutname').contains('Skinsheen Bronzer Stick');
+    })
+
+})
